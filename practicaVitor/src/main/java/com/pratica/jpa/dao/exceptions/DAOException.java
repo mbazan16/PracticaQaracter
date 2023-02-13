4dfaad949d459c161277d11090eda42e1e775585
VitorJCarmo <vitorblue2@hotmail.com>
2023-02-13 19:53:53 +0100

    Remove pratica

--- practicaVitor/src/main/java/com/pratica/jpa/dao/exceptions/DAOException.java
+++
@@ -1,9 +0,0 @@
-package com.pratica.jpa.dao.exceptions;
-
-@SuppressWarnings("serial")
-public class DAOException extends Exception {
-
-	public DAOException(Exception e) {
-		super(e.getMessage());
-	}
-}
